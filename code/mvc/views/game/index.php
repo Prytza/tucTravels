5f728004b8acf0b264fa74c833a497b77e10de13
Prytza <prytza@hotmail.com>
2012-09-19 15:38:12 +0200

    css andrad for spelkartan

--- code/mvc/views/game/index.php
+++ code/mvc/views/game/index.php
@@ -2,7 +2,8 @@
 
 		<h3>Från Din nuvarande plats till Golden Thai(närmsta systembolag) med helekopter...</h3>
     
-		<div id="map_canvas"></div>
-		<img id="helikopter" src="<?php echo URL; ?>public/images/helkoptercopy.png" alt="flygande helekopter" />
-	 
-		<p id="message">Starting...</p>
+		<div id="map_wrapper">
+			<div id="map_canvas"></div>
+			<img id="helikopter" src="<?php echo URL; ?>public/images/helkoptercopy.png" alt="flygande helekopter" />
+			<p id="message">Starting...</p>
+		</div>
