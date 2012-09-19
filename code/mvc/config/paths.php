471cc86b0827e34b0e3ab8bc2141cd99120adf74
Sofia Tudegard <sofias76@gmail.com>
2012-09-19 12:40:49 +0200

    uppdaterat game.js och game.php, Sofia

--- code/mvc/config/paths.php
+++ code/mvc/config/paths.php
@@ -1,5 +1,7 @@
 <?php
 
 //define("URL", "http://localhost/INTEGRATION/Projektarbete/mvc/");
+//Sofias --> http://localhost/php/integration/gitTucTravel/tucTravels/code/mvc/
 
-define("URL", "http://localhost/INTEGRATION/Projektarbete/tucTravels/tucTravels/code/mvc/");
+//define("URL", "http://localhost/INTEGRATION/Projektarbete/tucTravels/tucTravels/code/mvc/");
+define("URL", "http://localhost/php/integration/gitTucTravel/tucTravels/code/mvc/");
