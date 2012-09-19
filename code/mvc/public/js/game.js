c46e13be4b8e7d340b3d2513e4d29b4e62d89af2
Sofia Tudegard <sofias76@gmail.com>
2012-09-19 14:53:30 +0200

    sofia har gjort lite ändringar i game.js

--- code/mvc/public/js/game.js
+++ code/mvc/public/js/game.js
@@ -43,17 +43,24 @@ $(function () {
 	/*	
 	Systemet i linköping
 	58.4105226, 15.6271606
-	*/	
+	*/
+	var latEnd= 58.4105226;
+	var lngEnd= 15.6271606;
+
+	var foundStores = []; // önskade uppgifter om butiker
+
+		
+	/*
 //	Koordinats Golden Thai
 	var latEnd = 58.035609;
 	var lngEnd = 14.976296;
-	/*
+	
 	skäggetorp
 	58.4354436, 15.5882945
-	
+
 	var latEnd= 58.4354436;
 	var lngEnd= 15.5882945;
-*/
+*/	
 	var texten = "jag har inget att säga...";
 	//avstånd mellan varje zon
 	
@@ -78,14 +85,49 @@ $(function () {
 		latStart = mobilLat; 
 		lngStart = mobilLng;
 		
-		initialize();
+		var latitude = position.coords.latitude;
+		var longitude = position.coords.longitude;
+		
+		/*Hämtar uppgifter från systembevakningsagentens API*/
+		
+		$.getJSON("http://agent.nocrew.org/api/json/1.0/searchStore.json?callback=?", {
+			"lat": latitude,
+			"lng": longitude,
+			"dist_km": "5",
+			"limit" : "3"
+		}, function (data) {
+			
+			$.each(data.items, function (key, value) {
+			
+				foundStores[key] = [];
+		
+				foundStores[key]["id"] = value.id;
+				foundStores[key]["sysid"] = value.sysid;
+				foundStores[key]["lat"] = value.lat;
+				foundStores[key]["lng"] = value.lng;
+				foundStores[key]["address"] = value.address;
+				foundStores[key]["city"] = value.city;
+				foundStores[key]["dist_km"] = value.dist_km;	
+				
+			});
+			
+			console.log(foundStores);
+			
+			/*När uppgifterna är hämtade -> skriv ut kartan*/
+			var myLatlng = new google.maps.LatLng(latitude, longitude);
+			//printMap(myLatlng, "map_canvas");
+			initialize();
+		});
+	//}
+	
+		
+	//	initialize();
 
-	}
+	};
 
 	
 	//skapar kartan
 	function initialize() {
-	
 		var mapOptions = {
 			center: new google.maps.LatLng(latStart, lngStart),
 			zoom: 18,
@@ -94,8 +136,73 @@ $(function () {
 		
 		map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);	
 
+		/*markeringsikoner*/
+		var the_stores_logo = 'localhost/php/integration/gitTucTravel/tucTravels/code/mvc/public/js/helkoptercopy.png';
+		var points_where_you_are = 'lean.gif';
+		
+		/*Loopa för att placera ut alla butiker*/
+		for (var i = 0; i < foundStores.length; i++) {
+			var theStoresLocation = new google.maps.LatLng(foundStores[i].lat, foundStores[i].lng);
+			setMarkers(map, theStoresLocation, foundStores[i].address, the_stores_logo, foundStores[i].id);
+			
+		}
+		
+		//setMarkers(map, yourPosition, "Här är du!", points_where_you_are);
+	
 	};
+/*
+	function printMap (yourPosition, mapContainer) {
+			
+		var mapOptions = {
+			zoom: 10,
+			center: yourPosition,
+			mapTypeId: google.maps.MapTypeId.ROADMAP, 
+		}
+		
+		var map = new google.maps.Map(document.getElementById(mapContainer), mapOptions);
+		
+		/*markeringsikoner*/
+		/*
+		var the_stores_logo = 'helkoptercopy.png'
+		var points_where_you_are = 'helkoptercopy.png'
+		
+		/*Loopa för att placera ut alla butiker*/
+		/*for (var i = 0; i < foundStores.length; i++) {
+			var theStoresLocation = new google.maps.LatLng(foundStores[i].lat, foundStores[i].lng);
+			setMarkers(map, theStoresLocation, foundStores[i].address, the_stores_logo, foundStores[i].id);
+		}
+		setMarkers(map, yourPosition, "Här är du!", points_where_you_are);
+	}
+	*/
+	
+	
+	/*Sätt ut markörer enligt koordinaterna*/
+	function setMarkers (map, storeCoords, place, img, storeID = null) {	
+	
+		var marker = new google.maps.Marker({
+			position: storeCoords,
+			title: place,
+			//icon: img
+		});
+			
+				//alert("hej");
 
+		/*Lägg till markering till kartan*/
+		marker.setMap(map);
+		
+		/*Det som ska hända när du klickar på en ikon*/
+	/*	google.maps.event.addListener(marker, "click", function() {
+			if (storeID) {
+				alert(storeID);
+				b = new Beer();
+				var listOfBeers = b.getBeers(storeID);
+				console.log(listOfBeers);
+			}
+		});
+		*/
+	}
+	
+	
 	function leftArrowPressed(event){
 		event.preventDefault();	
 		//alert("left");		
@@ -128,21 +235,21 @@ $(function () {
 	
 	function getText(zon){
 		
-	//avstånd mellan varje zon
-		var changeTextLat = (latStart - latEnd) / 4;
-		var changeTextLng = (lngStart - lngEnd) / 4;
-		
-		if(changeTextLng < 0){
-			alert("mindre än noll...");
-		}
+	//avstånd mellan varje zon, 0.00068(lat/lng) och 0.000196(lat/lng) är ca 70 meter
+		//var changeTextLat = (latStart - latEnd) / 4;
+		//var changeTextLng = (lngStart - lngEnd) / 4;	
+	
+	//	if(changeTextLng < 0){
+	//		alert("mindre än noll...");
+	//	}
 	
 	//	alert(changeTextLat);
 	//	alert(changeTextLng);
 	
-		var zonCoord1 = latEnd + (changeTextLat * zon);
-		var zonCoord2 = latEnd - (changeTextLat * zon);
-		var zonCoord3 = lngEnd + (changeTextLng * zon);
-		var zonCoord4 = lngEnd - (changeTextLng * zon);
+		var zonCoord1 = latEnd + (0.00196 * zon);
+		var zonCoord2 = latEnd - (0.00196 * zon);
+		var zonCoord3 = lngEnd + (0.0068 * zon);
+		var zonCoord4 = lngEnd - (0.0068 * zon);
 		
 		if( mobilLat < zonCoord1 && mobilLng < zonCoord3 && mobilLat > zonCoord2 && mobilLng > zonCoord4 ){
 			
@@ -160,7 +267,7 @@ $(function () {
 //					alert("!! "+ zonCoord1);
 //				}
 	
-//			}
+//		}
 	
 	
 //		function writeText(zon){
@@ -190,9 +297,9 @@ $(function () {
 				texten = "Ooups! Du är ganska långt ifrån...";
 	
 			}
-		}
+		
 	
-//		}
+		}
 		return texten;
 	};	
 
--- code/mvc/views/game/index.php
+++ code/mvc/views/game/index.php
@@ -10,6 +10,6 @@
 		<img src="../../../mvc/public/images/helkoptercopy.png" style="position: absolute; left: 620px;top: 300px;" alt="flygande helekopter" height="40" width="40"/>
 		
 	-->
-		<img src="<?php echo URL; ?>/public/images/helkoptercopy.png" style="position: absolute; left: 700px;top: 650px;" alt="flygande helekopter" height="40" width="40"/>
+		<img src="<?php echo URL; ?>public/images/helkoptercopy.png" style="position: absolute; left: 700px;top: 650px;" alt="flygande helekopter" height="40" width="40"/>
 	 
 		<p id="message" style="position: absolute; left: 700px;top: 600px;">Starting...</p>
