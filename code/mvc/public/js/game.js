4266efbaaa80c0fce26916ffb68ca840af66d265
Sofia Tudegard <sofias76@gmail.com>
2012-09-20 14:50:07 +0200

    kommenterat bort systembolagets api eftersom det kranglar

--- code/mvc/public/js/game.js
+++ code/mvc/public/js/game.js
@@ -1,6 +1,6 @@
 $(function () {
 
-	alert(1);
+	//alert(1);
 
 	getLocation();
 	//initialize();
@@ -9,54 +9,55 @@ $(function () {
 		evt = evt || window.event;
 		switch (evt.keyCode) {
 			case 37:
+				//arrowPressed(evt,37);
 				leftArrowPressed(evt);
 				break;
 			case 38:
+				//arrowPressed(evt,38);
 				upArrowPressed(evt);
 				break;
 			case 39:
+				//arrowPressed(evt,39);
 				rightArrowPressed(evt);
 				break;
 			case 40:
+				//arrowPressed(evt,40);
 				downArrowPressed(evt);
 				break;	
 		}
 	};
 
-
 });
 
-/*
 	var mobilLat;
 	var mobilLng; 
 	var latStart; 
 	var lngStart;
-*/
-	
+	var map;
+
 	var mobilLat = 58.039818;
 	var mobilLng = 14.987047; 
-	
+/*	
 	//var latStart; //58.039818;//dessa koordinater ska vara dynamiska, 
 	//var lngStart; //14.987047;
 	
 	var latStart = mobilLat; 
 	var lngStart = mobilLng;
-
+*/
 	/*	
 	Systemet i linköping
 	58.4105226, 15.6271606
 	*/
-	var latEnd= 58.4105226;
+/*	var latEnd= 58.4105226;
 	var lngEnd= 15.6271606;
-
-	var foundStores = []; // önskade uppgifter om butiker
-
+*/
+//	var foundStores = []; // önskade uppgifter om butiker
 		
-	/*
+	
 //	Koordinats Golden Thai
 	var latEnd = 58.035609;
 	var lngEnd = 14.976296;
-	
+	/*
 	skäggetorp
 	58.4354436, 15.5882945
 
@@ -86,12 +87,12 @@ $(function () {
 	
 		latStart = mobilLat; 
 		lngStart = mobilLng;
-		
+/*
 		var latitude = position.coords.latitude;
 		var longitude = position.coords.longitude;
-		
+	*/	
 		/*Hämtar uppgifter från systembevakningsagentens API*/
-		
+		/*
 		$.getJSON("http://agent.nocrew.org/api/json/1.0/searchStore.json?callback=?", {
 			"lat": latitude,
 			"lng": longitude,
@@ -114,17 +115,15 @@ $(function () {
 			});
 			
 			console.log(foundStores);
-			
+			*/
 			/*När uppgifterna är hämtade -> skriv ut kartan*/
+			/*
 			var myLatlng = new google.maps.LatLng(latitude, longitude);
 			//printMap(myLatlng, "map_canvas");
-			initialize();
 		});
-	//}
+*/
+			initialize();
 	
-		
-	//	initialize();
-
 	};
 
 	
@@ -138,23 +137,36 @@ $(function () {
 		
 		map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 
-		alert(2);
+		//alert(22222);
 
 		/*markeringsikoner*/
+/*
 		var the_stores_logo = 'localhost/php/integration/gitTucTravel/tucTravels/code/mvc/public/js/helkoptercopy.png';
 		var points_where_you_are = 'lean.gif';
 		
+		var dist=500;
+	*/	
 		/*Loopa för att placera ut alla butiker*/
-		for (var i = 0; i < foundStores.length; i++) {
+/*		for (var i = 0; i < foundStores.length; i++) {
 			var theStoresLocation = new google.maps.LatLng(foundStores[i].lat, foundStores[i].lng);
 			setMarkers(map, theStoresLocation, foundStores[i].address, the_stores_logo, foundStores[i].id);
-			
+
+			if(dist > foundStores[i].dist_km){
+				//alert(foundStores[i].dist_km);
+				dist = foundStores[i].dist_km;
+				latEnd = foundStores[i].lat;
+				lngEnd = foundStores[i].lng;
+				alert(foundStores[i].lat + " --> latEnd var latEnd= 58.4105226;");
+				alert(foundStores[i].lng + " --> lngEnd var lngEnd= 15.6271606;");
+
+			}	
+				
 		}
 		
 		//setMarkers(map, yourPosition, "Här är du!", points_where_you_are);
-	
+	*/
 	};
-/*
+
 	function printMap (yourPosition, mapContainer) {
 			
 		var mapOptions = {
@@ -176,8 +188,9 @@ $(function () {
 			setMarkers(map, theStoresLocation, foundStores[i].address, the_stores_logo, foundStores[i].id);
 		}
 		setMarkers(map, yourPosition, "Här är du!", points_where_you_are);
-	}
 	*/
+	};
+	
 	
 	
 	/*Sätt ut markörer enligt koordinaterna*/
@@ -205,8 +218,22 @@ $(function () {
 		});
 		*/
 	}
-	
-	
+	/*
+	function arrowPressed(evt,keyNumber){
+		event.preventDefault();	
+		//alert("all");		
+		if(keyNumber==37){
+			mobilLng = mobilLng - 0.0001;
+		}else if(keyNumber==40){
+			mobilLat = mobilLat - 0.0001;
+		}else if(keyNumber==38){
+			mobilLat = mobilLat + 0.0001;
+		}else if(keyNumber==39){
+			mobilLng = mobilLng + 0.0001;
+		}
+		moveMap();
+	}
+	*/
 	function leftArrowPressed(event){
 		event.preventDefault();	
 		//alert("left");		
@@ -220,6 +247,7 @@ $(function () {
 		//alert("right");		
 		event.preventDefault();	
 		mobilLng = mobilLng + 0.0001;
+	//	alert(mobilLng);
 		moveMap();
 	};		
 
@@ -227,6 +255,7 @@ $(function () {
 	//alert("up");		
 		event.preventDefault();	
 		mobilLat = mobilLat + 0.0001;
+	//	alert(mobilLat);
 		moveMap();
 	};		
 
@@ -246,16 +275,25 @@ $(function () {
 	//	if(changeTextLng < 0){
 	//		alert("mindre än noll...");
 	//	}
+		var floor = Math.floor;
+	// var x = floor("1000.01")
+		/* alert(latEnd);
+		alert(lngEnd);
+	*/
+		var zonCoordOne = floor(latEnd)+ (0.0196 * zon);//öster om
+		var zonCoordTwo = latEnd - (0.0196 * zon);//väster om
+		var zonCoordThree = floor(lngEnd)+ (0.0068 * zon);//norr om
+		var zonCoordFour = lngEnd - (0.0068 * zon);//söder om
 	
-	//	alert(changeTextLat);
-	//	alert(changeTextLng);
-	
-		var zonCoord1 = latEnd + (0.00196 * zon);
-		var zonCoord2 = latEnd - (0.00196 * zon);
-		var zonCoord3 = lngEnd + (0.0068 * zon);
-		var zonCoord4 = lngEnd - (0.0068 * zon);
-		
-		if( mobilLat < zonCoord1 && mobilLng < zonCoord3 && mobilLat > zonCoord2 && mobilLng > zonCoord4 ){
+	/*	alert("mobilLat " + mobilLat +
+			 " mobilLng " + mobilLng + 
+			 " zonCoordOne " + zonCoordOne + 
+			 " zonCoordTwo " + zonCoordTwo 	+ 
+			 " zonCoordThree "+ zonCoordThree +  
+			 " zonCoordFour" + zonCoordFour 
+		);
+	*/
+		if( mobilLat < zonCoordOne &&  mobilLat > zonCoordTwo && mobilLng < zonCoordThree && mobilLng > zonCoordFour ){
 			
 //					writeText(zon);
 	
--- code/mvc/views/game/index.php
+++ code/mvc/views/game/index.php
@@ -1,7 +1,7 @@
-﻿<h2>Här ska själva spelet visas...</h2>
+﻿<!--<h2>Här ska själva spelet visas...</h2>
 
 		<h3>Från Din nuvarande plats till Golden Thai(närmsta systembolag) med helekopter...</h3>
-    
+    -->
 		<div id="map_canvas"></div>
 		<img id="helikopter" src="<?php echo URL; ?>public/images/helkoptercopy.png" alt="flygande helekopter" />
 	 
