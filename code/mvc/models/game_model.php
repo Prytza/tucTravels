850bb100f10f34ed81fb4d1cd020b13ea1fa4500
Prytza <prytza@hotmail.com>
2012-09-21 11:18:30 +0200

    Julius koordinatparametrar finns nu tillgangliga for sofia

---
+++ code/mvc/models/game_model.php
@@ -0,0 +1,33 @@
+﻿<?php
+
+class Game_Model extends Model {
+
+	function __construct() {
+		parent::__construct();
+	}
+	
+	function insert() 
+	{
+		//$text = $_POST['text'];
+		
+		$number = 444455555;
+		
+		
+		
+		$sth = $this->db->prepare('INSERT INTO user (facebookID) VALUES (:number)');
+		$sth->execute(array(':number' => $number));
+		
+		//$data = array('text' => $text, 'id' => $this->db->lastInsertId());
+		//echo json_encode($data);
+	}
+	
+	public function getDirections () {
+	
+		$sth = $this->db->prepare('SELECT * FROM mobil');
+		$sth->setFetchMode(PDO::FETCH_ASSOC);
+		$sth->execute();
+		$data = $sth->fetchAll();
+		echo json_encode($data);
+	}
+
+}
--- code/mvc/public/js/game.js
+++ code/mvc/public/js/game.js
@@ -1,5 +1,9 @@
 $(function () {
 
+	$.getJSON("game/getDirections", function (data) {
+		console.log(data);
+	});
+
 	getLocation();
 	//initialize();
 	
@@ -10,61 +14,75 @@ $(function () {
 		evt = evt || window.event;
 		switch (evt.keyCode) {
 			case 37:
+				//arrowPressed(evt,37);
 				leftArrowPressed(evt);
 				break;
 			case 38:
+				//arrowPressed(evt,38);
 				upArrowPressed(evt);
 				break;
 			case 39:
+				//arrowPressed(evt,39);
 				rightArrowPressed(evt);
 				break;
 			case 40:
+				//arrowPressed(evt,40);
 				downArrowPressed(evt);
-				break;	
+				break;
+		/*	case 37 && 38:
+				arrowPressed(evt,3738);
+				break;
+			case 38 && 39:
+				arrowPressed(evt,3839);
+				break;
+			case 39 && 40:
+				arrowPressed(evt,3940);
+				break;
+			case 40 && 37:
+				arrowPressed(evt,4037);
+				break;		
+		*/
 		}
 	};
 
-
 });
 
-/*
-	var mobilLat;
-	var mobilLng; 
+//	var mobilLat;
+//	var mobilLng; 
 	var latStart; 
 	var lngStart;
-*/
-	
+	var map;
+
 	var mobilLat = 58.039818;
 	var mobilLng = 14.987047; 
-	
+/*	
 	//var latStart; //58.039818;//dessa koordinater ska vara dynamiska, 
 	//var lngStart; //14.987047;
 	
 	var latStart = mobilLat; 
 	var lngStart = mobilLng;
-
+*/
 	/*	
 	Systemet i linköping
 	58.4105226, 15.6271606
 	*/
-	var latEnd= 58.4105226;
+/*	var latEnd= 58.4105226;
 	var lngEnd= 15.6271606;
-
-	var foundStores = []; // önskade uppgifter om butiker
-
+*/
+//	var foundStores = []; // önskade uppgifter om butiker
 		
-	/*
+	
 //	Koordinats Golden Thai
 	var latEnd = 58.035609;
 	var lngEnd = 14.976296;
-	
+	/*
 	skäggetorp
 	58.4354436, 15.5882945
 
 	var latEnd= 58.4354436;
 	var lngEnd= 15.5882945;
 */	
-	var texten = "jag har inget att säga...";
+	var texten = "hmm...";
 	//avstånd mellan varje zon
 	
 	function getLocation(){
@@ -87,12 +105,12 @@ $(function () {
 	
 		latStart = mobilLat; 
 		lngStart = mobilLng;
-		
+/*
 		var latitude = position.coords.latitude;
 		var longitude = position.coords.longitude;
-		
+	*/	
 		/*Hämtar uppgifter från systembevakningsagentens API*/
-		
+		/*
 		$.getJSON("http://agent.nocrew.org/api/json/1.0/searchStore.json?callback=?", {
 			"lat": latitude,
 			"lng": longitude,
@@ -115,17 +133,15 @@ $(function () {
 			});
 			
 			console.log(foundStores);
-			
+			*/
 			/*När uppgifterna är hämtade -> skriv ut kartan*/
+			/*
 			var myLatlng = new google.maps.LatLng(latitude, longitude);
 			//printMap(myLatlng, "map_canvas");
-			initialize();
 		});
-	//}
+*/
+			initialize();
 	
-		
-	//	initialize();
-
 	};
 
 	
@@ -139,23 +155,36 @@ $(function () {
 		
 		map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 
-		alert(2);
+		//alert(22222);
 
 		/*markeringsikoner*/
+/*
 		var the_stores_logo = 'localhost/php/integration/gitTucTravel/tucTravels/code/mvc/public/js/helkoptercopy.png';
 		var points_where_you_are = 'lean.gif';
 		
+		var dist=500;
+	*/	
 		/*Loopa för att placera ut alla butiker*/
-		for (var i = 0; i < foundStores.length; i++) {
+/*		for (var i = 0; i < foundStores.length; i++) {
 			var theStoresLocation = new google.maps.LatLng(foundStores[i].lat, foundStores[i].lng);
 			setMarkers(map, theStoresLocation, foundStores[i].address, the_stores_logo, foundStores[i].id);
-			
+
+			if(dist > foundStores[i].dist_km){
+				//alert(foundStores[i].dist_km);
+				dist = foundStores[i].dist_km;
+				latEnd = foundStores[i].lat;
+				lngEnd = foundStores[i].lng;
+				alert(foundStores[i].lat + " --> latEnd var latEnd= 58.4105226;");
+				alert(foundStores[i].lng + " --> lngEnd var lngEnd= 15.6271606;");
+
+			}	
+				
 		}
 		
 		//setMarkers(map, yourPosition, "Här är du!", points_where_you_are);
-	
+	*/
 	};
-/*
+
 	function printMap (yourPosition, mapContainer) {
 			
 		var mapOptions = {
@@ -177,8 +206,9 @@ $(function () {
 			setMarkers(map, theStoresLocation, foundStores[i].address, the_stores_logo, foundStores[i].id);
 		}
 		setMarkers(map, yourPosition, "Här är du!", points_where_you_are);
-	}
 	*/
+	};
+	
 	
 	
 	/*Sätt ut markörer enligt koordinaterna*/
@@ -206,8 +236,36 @@ $(function () {
 		});
 		*/
 	}
-	
-	
+	/*
+	function arrowPressed(evt,keyNumber){
+		event.preventDefault();	
+		//alert("all");		
+		if(keyNumber==37){
+			mobilLng = mobilLng - 0.0001;
+		}else if(keyNumber==40){
+			mobilLat = mobilLat - 0.0001;
+		}else if(keyNumber==38){
+			mobilLat = mobilLat + 0.0001;
+		}else if(keyNumber==39){
+			mobilLng = mobilLng + 0.0001;
+		}else if(keyNumber==3738){
+			mobilLng = mobilLng - 0.00005;
+			mobilLat = mobilLat + 0.00005;
+		}else if(keyNumber==3839){
+			mobilLat = mobilLat + 0.00005;
+			mobilLng = mobilLng	+ 0.00005;
+		}else if(keyNumber==3940){
+			mobilLat = mobilLat - 0.00005;
+			mobilLng = mobilLng + 0.00005;
+		}else if(keyNumber==4037){
+			mobilLat = mobilLat - 0.00005;
+			mobilLng = mobilLng + 0.00005;
+		}
+		
+		moveMap();	
+		
+	}
+	*/
 	function leftArrowPressed(event){
 		event.preventDefault();	
 		//alert("left");		
@@ -221,6 +279,7 @@ $(function () {
 		//alert("right");		
 		event.preventDefault();	
 		mobilLng = mobilLng + 0.0001;
+	//	alert(mobilLng);
 		moveMap();
 	};		
 
@@ -228,6 +287,7 @@ $(function () {
 	//alert("up");		
 		event.preventDefault();	
 		mobilLat = mobilLat + 0.0001;
+	//	alert(mobilLat);
 		moveMap();
 	};		
 
@@ -240,42 +300,23 @@ $(function () {
 	
 	function getText(zon){
 		
-	//avstånd mellan varje zon, 0.00068(lat/lng) och 0.000196(lat/lng) är ca 70 meter
-		//var changeTextLat = (latStart - latEnd) / 4;
-		//var changeTextLng = (lngStart - lngEnd) / 4;	
+		var floor = Math.floor;
+
+		var zonCoordOne = floor(latEnd)+ (0.0196 * zon);//öster om
+		var zonCoordTwo = latEnd - (0.0196 * zon);//väster om
+		var zonCoordThree = floor(lngEnd)+ (0.0068 * zon);//norr om
+		var zonCoordFour = lngEnd - (0.0068 * zon);//söder om
 	
-	//	if(changeTextLng < 0){
-	//		alert("mindre än noll...");
-	//	}
-	
-	//	alert(changeTextLat);
-	//	alert(changeTextLng);
-	
-		var zonCoord1 = latEnd + (0.00196 * zon);
-		var zonCoord2 = latEnd - (0.00196 * zon);
-		var zonCoord3 = lngEnd + (0.0068 * zon);
-		var zonCoord4 = lngEnd - (0.0068 * zon);
-		
-		if( mobilLat < zonCoord1 && mobilLng < zonCoord3 && mobilLat > zonCoord2 && mobilLng > zonCoord4 ){
+	/*	alert("mobilLat " + mobilLat +
+			 " mobilLng " + mobilLng + 
+			 " zonCoordOne " + zonCoordOne + 
+			 " zonCoordTwo " + zonCoordTwo 	+ 
+			 " zonCoordThree "+ zonCoordThree +  
+			 " zonCoordFour" + zonCoordFour 
+		);
+	*/
+		if( mobilLat < zonCoordOne &&  mobilLat > zonCoordTwo && mobilLng < zonCoordThree && mobilLng > zonCoordFour ){
 			
-//					writeText(zon);
-	
-//					alert("!! "+ zonCoord1);
-//				}
-		
-//			}else{
-		
-//				if( mobilLat > zonCoord1 && mobilLng > zonCoord3 && mobilLat < zonCoord2 && mobilLng < zonCoord4 ){
-	
-//					writeText(zon);
-	
-//					alert("!! "+ zonCoord1);
-//				}
-	
-//		}
-	
-	
-//		function writeText(zon){
 	
 			if(zon == 4){
 			
@@ -311,7 +352,6 @@ $(function () {
 
 	function moveMap(){	
 	
-	//alert(2);
 		texten = getText(4);
 		texten = getText(3);
 		texten = getText(2);
