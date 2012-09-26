f7da8603f4ffd45626bc3f2c3c83b127cf1969f8
Prytza <prytza@hotmail.com>
2012-09-26 10:23:52 +0200

    sista ignore forsoket

---
+++ code/tuctravels/config/database.php
@@ -0,0 +1,14 @@
+<?php
+
+define("DB_TYPE", "mysql");
+// define("DB_HOST", "tuc-travels-142080.mysql.binero.se");
+define("DB_HOST", "localhost:9999");
+define("DB_NAME", "142080-tuc-travels");
+define("DB_USER", "142080_jj39999");
+define("DB_PASS", "Travels3");
+
+// define("DB_TYPE", "mysql");
+// define("DB_HOST", "localhost");
+// define("DB_NAME", "projektarbete_integration");
+// define("DB_USER", "root");
+// define("DB_PASS", "");
